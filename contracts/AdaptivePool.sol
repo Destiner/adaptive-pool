3093b98e8202fb89dab6581cc76e09e8e104c170
Timur Badretdinov <destinerx@gmail.com>
2021-06-25 09:45:11 +0300

    Inherit BPT and base pool logic for pool

--- contracts/AdaptivePool.sol
+++ contracts/AdaptivePool.sol
@@ -1,5 +1,12 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.7.1;
+pragma experimental ABIEncoderV2;
 
+import "@balancer-labs/v2-vault/contracts/interfaces/IMinimalSwapInfoPool.sol";
 import "@balancer-labs/v2-vault/contracts/interfaces/IVault.sol";
+import "@balancer-labs/v2-pool-utils/contracts/BalancerPoolToken.sol";
+
+abstract contract AdaptivePool is IMinimalSwapInfoPool, BalancerPoolToken {
+
+}
